c16c14c0d4a82a75f1f923733a376b2f5742e8ff
zacharyrb99 <zach.boudreaux999@gmail.com>
2021-10-20 02:00:18 -0400

    finished graphs

--- graph.js
+++ graph.js
@@ -11,25 +11,86 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.addVertex(vertex);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    for (let node of this.nodes) {
+      if (node.adjacent.has(vertex)) {
+        node.adjacent.delete(vertex);
+      }
+    }
+
+    this.nodes.delete(vertex);
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    let seen = new Set();
+    let vertexArr = [];
+
+    const traverse = vertex => {
+      if (!vertex) return null;
+
+      seen.add(vertex);
+      vertexArr.push(vertex.value);
+
+      vertex.adjacent.forEach(neighbor => {
+        if (!seen.has(neighbor)) {
+          return traverse(neighbor);
+        }
+      })
+    }
+
+    traverse(start);
+
+    return vertexArr;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    let queue = [start];
+    let vertexArr = [];
+    let seen = new Set();
+    let currVertex;
+
+    seen.add(start);
+    
+    while(queue.length) {
+      currVertex = queue.shift();
+      vertexArr.push(currVertex.value);
+
+      currVertex.adjacent.forEach(neighbor => {
+        if (!seen.has(neighbor)) {
+          seen.add(neighbor);
+          queue.push(neighbor);
+        }
+      });
+    }
+
+    return vertexArr;
+  }
 }
 
 module.exports = {Graph, Node}
